8e6a3590bc90f397dd21d6ff259be7af77da2d77
Thomega35 <thomega35@gmail.com>
2024-10-13 17:34:12 +0200

    Add Hello World exemple in go

---
+++ code/hello.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// main is the entry point of the program
+func main() {
+	// Call the Hello function
+	Hello()
+}
+
+// Hello prints a greeting message to the console
+func Hello() {
+	fmt.Println("Hello, World!")
+}
